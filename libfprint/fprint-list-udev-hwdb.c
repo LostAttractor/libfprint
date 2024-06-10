abf8dffabfcc7373d4562c151d41a6c7e087ee41
ChaosAttractor <lostattractor@gmail.com>
2024-06-11 01:14:05 +0800

    fixup: remove 0x538d in udev-hwdb

--- libfprint/fprint-list-udev-hwdb.c
+++ libfprint/fprint-list-udev-hwdb.c
@@ -120,7 +120,6 @@ static const FpIdEntry allowlist_id_table[] = {
   { .vid = 0x27c6, .pid = 0x5381 },
   { .vid = 0x27c6, .pid = 0x5385 },
   { .vid = 0x27c6, .pid = 0x538c },
-  { .vid = 0x27c6, .pid = 0x538d },
   { .vid = 0x27c6, .pid = 0x5395 },
   { .vid = 0x27c6, .pid = 0x5503 },
   { .vid = 0x27c6, .pid = 0x550a },
